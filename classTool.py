6a40506abcbfd6047587890ee746fada2a1ac21f
Eden <edenraf@hotmail.com>
2021-05-31 05:20:33 +0300

    added new feature send messages to people i liked

--- classTool.py
+++ classTool.py
@@ -1,5 +1,9 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class OkCupidSelenium:
     def __init__(self, user_name, password, PATH):
@@ -30,6 +34,7 @@ class OkCupidSelenium:
         search.click()
         time.sleep(10)
 
+
     def like(self, driver):
         search = driver.find_element_by_class_name("likes-pill-button-inner")
         search.click()
@@ -39,6 +44,68 @@ class OkCupidSelenium:
         search = driver.find_element_by_class_name("pass-pill-button-inner")
         search.click()
         time.sleep(5)
+
+    def message(self, driver, contain, people):
+        # search = driver.find_element_by_xpath("/html/body/div[1]/main/nav/div/span[1]/a[4]/div/span")
+        # print("0")
+        # search = driver.find_element_by_xpath("//*[@id=\"nav_ratings\"]")
+        # search.click()
+        # print("1")
+        while(people > 0):
+            cnt = 0
+            print("people are => {}".format(people))
+            search = driver.find_element_by_xpath("//*[@id=\"nav_ratings\"]")
+            search.click()
+            try:
+                search2 = WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.XPATH, "//*[@id=\"userRows-app\"]/section/div/div/span[3]/a"))
+                )
+                search2.click()
+                print("here")
+                time.sleep(7)
+                search3 = driver.find_element_by_xpath(
+                    "//*[@id=\"userRows-app\"]/div/main/div/div/div/div[2]/div[1]/div[1]/div")
+                search3.click()
+                time.sleep(4)
+                print("will check it now!")
+                search4 = driver.find_elements_by_class_name("profile-pill-buttons")
+                for s in search4:
+                    if(cnt == 0):
+                        cnt += 1
+                    else:
+                        k = s.find_element_by_xpath(
+                            "/html/body/div[1]/main/div[1]/div[2]/div/div/div[3]/span/div/button[2]")
+                        k.click()
+                        time.sleep(4)
+
+                m = driver.find_element_by_xpath(
+                    "/html/body/div[1]/main/div[1]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[2]/textarea")
+                m.send_keys(contain)
+                m = driver.find_element_by_xpath(
+                    "//*[@id=\"main_content\"]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[3]/button")
+                m.click()
+                time.sleep(4)
+                m = driver.find_element_by_xpath(
+                    "//*[@id=\"main_content\"]/div[4]/div[2]/div[2]/div/div/div/div/div/div/div[1]/button/span")
+                m.click()
+                driver.refresh()
+                time.sleep(4)
+                people -= 1
+
+            finally: pass
+            # print("2")
+            # #search2 = driver.find_element_by_xpath("//*[@id=\"userRows-app\"]/section/div/div/span[3]/a")
+            # time.sleep(3)
+            # search3 = driver.find_element_by_xpath("//*[@id=\"userRows-app\"]/div/main/div/div/div/div[2]/div[1]/div[1]/div")
+            # search3.click()
+            # print("3")
+            # time.sleep(3)
+
+
+            # search = driver.find_element_by_class_name("pass-pill-button-inner")
+            # search.click()
+            # time.sleep(5)
+
 
     def location(self, driver):
         search_location = driver.find_element_by_class_name("cardsummary-location")
--- main.py
+++ main.py
@@ -8,26 +8,34 @@ if __name__ == "__main__":
     saved_locations = ["`Akko", "Qiryat Yam", "Haifa", "Qiryat Motzkin", "Qiryat Bialik", "`Afula", "Yoqne`am",
                        "Or `Aqiva", "Tiberias", "Shelomi", "Karmi’el", "Ma`alot", "Hadar HaKarmel"]
     PATH = "C:\Program Files (x86)\chromedriver.exe"
-    user_name = "name@domain.com"
+    user_name = "name@hotmail.com"
     password = "password"
     ok = OkCupidSelenium(user_name, password, PATH)
+    contain = "*משחק אותה שלא מתלהב בכלל* מה קורה?"
+    people = 300
     while(True):
         driver = ok.getDriver()
         ok.login(driver)
-        while(check_counter):
-            location = ok.location(driver)
-            print(location)
-            for s in saved_locations:
-                if(location == s):
-                    like_counter += 1
-                    print("like: {} people\n".format(like_counter))
-                    ok.like(driver)
-                    continue
-            ok.dislike(driver)
-            dislike_counter += 1
-            check_counter -= 1
-        print("the like counter is: {}\n".format(like_counter))
-        print("the dislike counter is: {}\n".format(dislike_counter))
+        ######################################################################
+        # method 1: like dislike method
+        # while(check_counter):
+        #     location = ok.location(driver)
+        #     print(location)
+        #     for s in saved_locations:
+        #         if(location == s):
+        #             like_counter += 1
+        #             print("like: {} people\n".format(like_counter))
+        #             ok.like(driver)
+        #             continue
+        #     ok.dislike(driver)
+        #     dislike_counter += 1
+        #     check_counter -= 1
+        # print("the like counter is: {}\n".format(like_counter))
+        # print("the dislike counter is: {}\n".format(dislike_counter))
+        ####################################################################
+
+        ok.message(driver, contain, people)
+        classTool.time.sleep(100)
         ok.close(driver)
 
 
